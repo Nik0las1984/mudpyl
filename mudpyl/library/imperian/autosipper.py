1d7e8c9730edd97cfc90eb4e1163c5608785482e
Sam Pointon <sampointon@gmail.com>
2008-02-07 20:35:32 +0000

    make autosipper do mana too, reorganise TODO

--- mudpyl/library/imperian/autosipper.py
+++ mudpyl/library/imperian/autosipper.py
@@ -13,10 +13,11 @@ class Autosipper(EarlyInitialisingModule):
 
     def __init__(self, max_health, max_mana):
         self.max_health = max_health
+        self.max_mana = max_mana
         self.health_threshold = 0
+        self.mana_threshold = 0
         self.state = 'sip balance'
         self.calculate_threshold()
-        #TODO: expand to do mana as well
 
     @binding_trigger('^H:(\d+) M:(\d+) E:\d+ W:\d+ <.*>')
     def prompt_seen(self, match, realm):
@@ -28,15 +29,21 @@ class Autosipper(EarlyInitialisingModule):
         if self.state == 'sip balance':
             if health <= self.health_threshold:
                 realm.send('drh')
-            #TODO: mana here too
+            elif mana <= self.mana_threshold:
+                realm.send('drm')
 
     @binding_alias('^drh$')
     def sip_health(self, match, realm):
         """Sip from our health vial."""
-        #TODO: don't be so trusting. Add a timeout and a trigger for when we
-        #actually sip
         self.state = 'off balance'
         realm.send('drink health')
+        realm.send_to_mud = False
+
+    @binding_alias("^drm$")
+    def sip_mana(self, match, realm):
+        """Sip from our mana vial."""
+        self.state = 'off balance'
+        realm.send("drink mana")
         realm.send_to_mud = False
 
     @binding_trigger('^You may drink another healing elixir\.$')
@@ -51,17 +58,27 @@ class Autosipper(EarlyInitialisingModule):
         self.max_health = int(match.group(1))
         self.calculate_threshold()
 
+    @binding_trigger('^ Mana     : \d+/(\d+)\s*Reserves : \d+/\d+$')
+    def mana_update(self, match, realm):
+        """Recalcuate our sipping threshold based on SCORE."""
+        self.max_mana = int(match.group(1))
+        self.calculate_threshold()
+
     @property
     def triggers(self):
         """The triggers we want added."""
-        return [self.prompt_seen, self.got_balance, self.health_update]
+        return [self.prompt_seen, self.got_balance, self.health_update,
+                self.mana_update]
 
     @property
     def aliases(self):
         """The aliases we want added."""
-        return [self.sip_health]
+        return [self.sip_health, self.sip_mana]
 
     def calculate_threshold(self):
         """Calculate the threshold where we should start sipping at."""
-        sip = (self.max_health * 11 * 0.15 + 100) / 11
-        self.health_threshold = self.max_health - sip
+        healthsip = (self.max_health * 11 * 0.15 + 100) / 11
+        self.health_threshold = self.max_health - healthsip
+
+        manasip = (self.max_mana * 11 * 0.15 + 100) / 11
+        self.mana_threshold = self.max_mana - manasip
