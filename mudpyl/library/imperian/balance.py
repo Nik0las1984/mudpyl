4e40bd584d9e6fb8b92b19d43cab25d901577d51
Sam Pointon <sampointon@gmail.com>
2008-02-10 21:48:08 +0000

    balance highlighter for Imperian

--- mudpyl/library/imperian/balance.py
+++ mudpyl/library/imperian/balance.py
@@ -1,17 +1,16 @@
 """A balance highlighter.
 
-For: Achaea.
+For: Imperian.
 """
-from mudpyl.colours import HexFGCode
 from mudpyl.triggers import non_binding_trigger
+from mudpyl.colours import HexFGCode
 
 #The unused arguments are harmless.
 #pylint: disable-msg=W0613
 
-@non_binding_trigger("^You have recovered (?:equilibrium|balance on all "
-                                                                  "limbs)\.$")
+@non_binding_trigger("^You have recovered balance\.$")
 def balance_highlight(match, realm):
-    """When we get balance or equilibrium back, set it to a garish green."""
+    """When we get balance back, set it to a garish green."""
     realm.alterer.change_fore(0, match.end(), HexFGCode(0x80, 0xFF, 0x80))
 
 #pylint: enable-msg=W0613
---
+++ mudpyl/library/imperian/tests/test_balance.py
@@ -0,0 +1,51 @@
+from mudpyl.library.imperian.balance import balance_highlight
+from mudpyl.metaline import Metaline, RunLengthList
+from mudpyl.colours import HexFGCode
+from mudpyl.triggers import LineAlterer
+from mudpyl.realms import RootRealm, TriggerMatchingRealm
+import sys
+import re
+
+class FakeTelnet:
+
+    def __init__(self):
+        self.sent = []
+
+    def sendLine(self, line):
+        self.sent.append(line)
+
+class FakeOutputManager:
+
+    def __init__(self):
+        self.fore = object()
+        self.back = object()
+        self.written = []
+
+    def write_to_screen(self, metaline):
+        self.written.append(metaline)
+
+class FakeFactory:
+
+    def __init__(self):
+        self.outputs = FakeOutputManager()
+
+class Test_balance_highlight:
+
+    def setUp(self):
+        self.trig = balance_highlight()
+
+    def test_matches_on_balance(self):
+        assert self.trig.match(Metaline("You have recovered balance.", None,
+                                        None))
+
+    def test_highlights(self):
+        m = Metaline("foo", RunLengthList([(0, 'foo')]), 
+                     RunLengthList([(0, 'bar')]))
+        root = RootRealm(FakeFactory())
+        realm = TriggerMatchingRealm(m, root, root)
+        match = re.search('foobar', 'foobar')
+        self.trig.func(match, realm)
+        res = realm.alterer.apply(m)
+        assert res.fores.as_pruned_index_list() == \
+                [(0, HexFGCode(0x80, 0xFF, 0x80)),
+                 (6, 'foo')]
